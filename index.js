a7c1c7967eda9395a8a0a2ffcf2f3c1ad2e524ab
saruaralam-swd <saruaralam2001@gmail.com>
2022-11-25 02:34:21 +0600

    add collections

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0269g6x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// middleware
+function verifyJwt(req, res, next) {
+  const header = req.headers.authorization;
+
+  if (!header) {
+    return res.status(401).send({ message: 'unauthorized user', statusCode: 401 });
+  }
+
+  const token = header.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (error, decoded) {
+    if (error) {
+      return res.status(403).send({ message: 'forbidden access', statusCode: 403 })
+    }
+
+    req.decoded = decoded;
+    next()
+  })
+};
+
 
 async function run() {
   try {
@@ -28,13 +48,38 @@ async function run() {
 run().catch(error => { console.log(error.name, error.message) })
 
 // --------------------------- collection --------------------------->
+const usersCollection = client.db('usedProductResale').collection('users');
 const categoriesCollection = client.db('usedProductResale').collection('categories');
+const productsCollection = client.db('usedProductResale').collection('products');
 
+
+
+// ---------------------> users
+app.post('/users', async (req, res) => {
+  const user = req.body;
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+});
+
+
+
+// ---------------------> categories
 app.get('/categories', async (req, res) => {
   const query = {};
   const result = await categoriesCollection.find(query).toArray();
   res.send(result)
 });
+
+
+// ---------------------> products
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Used Product server is running')
