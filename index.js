958a03c83bdaccdd5a695aed01b52226dbe52049
saruaralam-swd <saruaralam2001@gmail.com>
2022-11-28 00:50:05 +0600

    specific product delete

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0269g6x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// middleware
+// middleware (verify jwt)
 function verifyJwt(req, res, next) {
   const header = req.headers.authorization;
 
@@ -54,27 +54,18 @@ const categoriesCollection = client.db('usedProductResale').collection('categori
 const productsCollection = client.db('usedProductResale').collection('products');
 const ordersCollection = client.db('usedProductResale').collection('orders');
 
-app.get('/jwt', async (req, res) => {
-  const email = req.query.email;
-  const query = { email: email };
-  const user = await usersCollection.findOne(query);
-
-  if (user) {
-    const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '20d' });
-    return res.send({ accessToken: token });
-  }
-
-  res.status(403).send({ accessToken: '' });
-});
-
 
 const verifyAdmin = async (req, res, next) => {
   const decodedEmail = req.decoded.email;
-  const query = { email: decodedEmail }
-  const user = await usersCollection.findOne(query);
+  const email = req.query.email;
+  if (decodedEmail !== email) {
+    return res.status(403).send({ message: 'forbidden access' })
+  }
 
-  if (user?.role !== 'admin') {
-    return res.status(403).send({ acknowledgement: false })
+  const query = { email };
+  const user = await usersCollection.findOne(query);
+  if (user?.role !== "admin") {
+    return res.status(403).send({ message: 'forbidden access' })
   }
 
   next();
@@ -82,11 +73,16 @@ const verifyAdmin = async (req, res, next) => {
 
 const verifySeller = async (req, res, next) => {
   const decodedEmail = req.decoded.email;
-  const query = { email: decodedEmail }
-  const user = await usersCollection.findOne(query);
+  const email = req.query.email;
 
-  if (user?.role !== 'seller') {
-    return res.status(403).send({ acknowledgement: false })
+  if (decodedEmail !== email) {
+    return res.status(403).send({ message: 'forbidden access' })
+  }
+
+  const query = { email };
+  const user = await usersCollection.findOne(query);
+  if (user?.role !== "seller") {
+    return res.status(403).send({ message: 'forbidden access' })
   }
 
   next();
@@ -104,16 +100,29 @@ const verifyBearer = async (req, res, next) => {
   next();
 };
 
-// ---------------------> users
+// # users
 app.post('/users', async (req, res) => { // store user info in Data base
   const user = req.body;
   const result = await usersCollection.insertOne(user);
   res.send(result);
 });
 
+app.get('/jwt', async (req, res) => {
+  const email = req.query.email;
+  const query = { email: email };
+  const user = await usersCollection.findOne(query);
+
+  if (user) {
+    const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '20d' });
+    return res.send({ accessToken: token });
+  }
+
+  res.status(403).send({ accessToken: '' });
+});
 
 
-// ---------------------> categories
+
+// categories
 app.get('/categories', async (req, res) => {
   const query = {};
   const result = await categoriesCollection.find(query).toArray();
@@ -128,11 +137,18 @@ app.get('/category/:id', async (req, res) => {
 });
 
 
-
-// ---------------------> products
-app.post('/product', async (req, res) => {
+// -------> products
+app.post('/product', verifyJwt, verifySeller, async (req, res) => {
   const data = req.body;
   const result = await productsCollection.insertOne(data);
+  res.send(result);
+});
+
+
+app.get('/myProducts', verifyJwt, verifySeller, async (req, res) => {
+  const email = req.query.email;
+  const query = { sellerEmail: email };
+  const result = await productsCollection.find(query).toArray();
   res.send(result);
 });
 
@@ -153,24 +169,42 @@ app.put('/products/:id', async (req, res) => { // for advertise
 });
 
 
-app.get('/products', async (req, res) => {
-  const query = {};
-  const result = await productsCollection.find(query).toArray();
+app.delete('/product/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await productsCollection.deleteOne(query);
   res.send(result);
 });
+
 
 
 app.get('/advertisement', async (req, res) => {
-  const query = { advertise: true, available: true }
+  const query = { advertise: true, }
   const result = await productsCollection.find(query).toArray();
   res.send(result);
 });
 
 
+app.get('/myBuyers', verifyJwt, verifySeller, async (req, res) => {
+  const email = req.query.email;
+  const query = { sellerEmail: email };
+  const result = await ordersCollection.find(query).toArray();
+  res.send(result)
+});
 
-// ---------------------> orders
+
+
+
+
+// -------> orders
 // when order success, product available: false
-app.get('/orders', async (req, res) => {
+app.post('/order', async (req, res) => {
+  const order = req.body;
+  const result = await ordersCollection.insertOne(order);
+  res.send(result);
+});
+
+app.get('/orders', verifyJwt, async (req, res) => {
   const email = req.query.email;
   const query = { email };
   const result = await ordersCollection.find(query).toArray();
@@ -192,59 +226,20 @@ app.put('/available/:id', async (req, res) => {
 });
 
 
-app.post('/order', async (req, res) => {
-  const order = req.body;
-  const result = await ordersCollection.insertOne(order);
-  res.send(result);
-});
 
 
-// ---------------------> myBuyers
-app.get('/myBuyers', verifyJwt, async (req, res) => {
-  // const email = req.query.email;
-  // const filter = { email };
-  // const user = await usersCollection.findOne(filter);
-
-  // if (user?.role !== "seller") {
-  //   return res.status(403).send({ message: 'forbidden access' })
-  // }
-
-  // const query = { sellerEmail: email };
-  const query = {};
-  const result = await ordersCollection.find(query).toArray();
-  res.send(result)
-});
-
-app.get('/allBuyers', async (req, res) => {
-  // const email = req.query.email;
-  // const filter = { email };
-  // const user = await usersCollection.findOne(filter);
-
-  // if (user?.role !== "admin") {
-  //   return res.status(403).send({ message: 'forbidden access' })
-  // }
-
+// ---------------------> for admin
+app.get('/allBuyers', verifyJwt, verifyAdmin, async (req, res) => {
   const query = {};
   const result = await ordersCollection.find(query).project({ email: 1 }).toArray();
   res.send(result)
 });
 
-
-// ---------------------> sellers
-app.get('/mySellers', async (req, res) => {
-  // const email = req.query.email;
-  // const filter = { email };
-  // const user = await usersCollection.findOne(filter);
-
-  // if (user?.role !== "admin") {
-  //   return res.status(403).send({ message: 'forbidden access' })
-  // }
-
+app.get('/allSellers', verifyJwt, verifyAdmin, async (req, res) => {
   const query = {};
   const result = await productsCollection.find(query).project({ sellerEmail: 1, sellerName: 1, verify: 1, }).toArray();
   res.send(result);
 });
-
 
 
 // ----> check user role <----
@@ -271,7 +266,6 @@ app.get('/user/buyer/:email', async (req, res) => { // check user is buyer
 
 
 
-// <------------------->
 app.get('/', (req, res) => {
   res.send('Used Product server is running')
 });
@@ -280,3 +274,32 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`server running on the port ${port}`.cyan)
 });
+
+
+/**
+ * user
+ *  category
+ * allCategory + category/id (display all products)
+
+ * product
+
+ * order
+ * product order
+
+ * check user role
+ * admin > all buyer + all seller + reported product
+ * seller > myProducts + addProduct + myBuyers
+ * buyer > myOrder + wishlist
+*/
+
+// app.get('/addPrice', async (req, res) => {
+//   const filter = {};
+//   const options = { upsert: true };
+//   const updateDoc = {
+//     $set: {
+//       available: true
+//     }
+//   }
+//   const result = await productsCollection.updateMany(filter, updateDoc, options);
+//   res.send(result)
+// });
